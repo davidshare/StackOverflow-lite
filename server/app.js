8ee62d54c05d40be7bd0bd686cad37c369df5a98
davidshare <davidessienshare@gmail.com>
2018-08-16 09:55:50 +0100

    Setup node development environment
    Setup node environment for app:
    - Setup babel
    - Setup linting with eslint
    - Set gitignore for node modules
    - Create base route

---
+++ server/app.js
@@ -0,0 +1,16 @@
+import Express from 'express';
+import bodyParser from 'body-parser';
+
+const app = new Express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+
+app.get('/', (req, res) => res.send({
+    "message":"Welcome to StackOverflow-lite"
+}));
+
+app.listen(3000, () => console.log(`Listening on port ${port}`));
