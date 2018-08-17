e238bf403f981045886c5fb56db802da4bbbe09f
davidshare <davidessienshare@gmail.com>
2018-08-17 02:37:16 +0100

    Fix indentation according to airbnb style
    Set indentation for files to carry two spaces instead of four
    in order to satisfy the airbnb style guide for indentation in
    javascript

--- server/app.js
+++ server/app.js
@@ -1,22 +1,22 @@
-//import modules
+// import modules
 import Express from 'express';
 import bodyParser from 'body-parser';
 
-//declare constants
+// declare constants
 const app = new Express();
 const port = process.env.PORT || 3000;
 
-//declare middleware
+// declare middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false,
+  extended: false,
 }));
 
-//declare 404 route
+// declare 404 route
 app.all('*', (req, res) => res.status(404).json({
-    message: 'The URL you are trying to access does not exist. Please enter a valide url',
-    status: 'fail',
+  message: 'The URL you are trying to access does not exist. Please enter a valide url',
+  status: 'fail',
 }));
 
-//listen to app port
+// listen to app port
 app.listen(3000, () => console.log(`Listening on port ${port}`));
