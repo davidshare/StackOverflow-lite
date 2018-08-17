0df14805513217cdbcc910b8884ecf921aad0cb6
davidshare <davidessienshare@gmail.com>
2018-08-17 02:25:34 +0100

    Create base route for app
    Remove the home route from the entry point - app.js and create
    a new route to handle all urls that do not match defined routes
    add comments for files

--- server/app.js
+++ server/app.js
@@ -1,16 +1,22 @@
+//import modules
 import Express from 'express';
 import bodyParser from 'body-parser';
 
+//declare constants
 const app = new Express();
 const port = process.env.PORT || 3000;
 
+//declare middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false,
 }));
 
-app.get('/', (req, res) => res.send({
-    message: 'Welcome to StackOverflow-lite',
+//declare 404 route
+app.all('*', (req, res) => res.status(404).json({
+    message: 'The URL you are trying to access does not exist. Please enter a valide url',
+    status: 'fail',
 }));
 
+//listen to app port
 app.listen(3000, () => console.log(`Listening on port ${port}`));
