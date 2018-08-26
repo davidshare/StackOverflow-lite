b437d4fe28c348106eb6a5ae6b99615e204f6dbe
David Itam Essien <davidessienshare@gmail.com>
2018-08-26 06:26:05 +0100

    Revert "#160020666 "

--- server/models/db.destroy.js
+++
@@ -1,8 +0,0 @@
-const answersDestroy = 'DROP TABLE IF EXISTS answers';
-const questionsDestroy = 'DROP TABLE IF EXISTS questions';
-const usersDestroy = 'DROP TABLE IF EXISTS users';
-const dbDestroy = 'DROP DATABASE IF EXISTS stacklite';
-
-const destroyQuery = `${answersDestroy}${questionsDestroy}${usersDestroy}${dbDestroy}`;
-
-export default destroyQuery;
